967f2f62ae5ce0dc5b06f9f58cdc1aeb5e258d65
kdrangej <kdrangej@users.noreply.github.com>
2018-03-21 13:03:44 +0100

    Create aliases.bat

---
+++ DOS/aliases.bat
@@ -0,0 +1,14 @@
+doskey clear=cls
+doskey ls=dir $* /ON
+doskey ll=dir $* /ON
+doskey man=help $*
+doskey cat=type $*
+doskey which=where $*
+doskey mv=move $*
+doskey cp=copy $*
+doskey pwd=%homedrive%$Tcd%homepath%
+doskey ed="c:\Program Files (x86)\Notepad++\notepad++.exe" $*
+doskey nano=ed
+doskey pico=ed
+doskey alias=doskey /macros
+doskey cscwin=csc /target:winexe $*
